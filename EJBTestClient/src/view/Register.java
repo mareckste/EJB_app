98e605ca11fa3bdb34c86e3911cf6d4f98f8c997
MareCK <stevuliak.m@gmail.com>
2016-05-12 18:47:25 +0200

    Nacrt GUI 01

---
+++ EJBTestClient/src/view/Register.java
@@ -0,0 +1,17 @@
+package view;
+
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Register extends JFrame{
+	public Register() {
+		getContentPane().setLayout(null);
+		
+		JLabel lblMeno = new JLabel("Meno");
+		lblMeno.setBounds(24, 33, 46, 14);
+		getContentPane().add(lblMeno);
+	}
+
+	
+}
---
+++ EJBTestClient/src/view/testGUI.java
@@ -0,0 +1,9 @@
+package view;
+
+public class testGUI {
+
+	public static void main(String[] args) {
+		new LoginPW().setVisible(true);;
+	}
+
+}
